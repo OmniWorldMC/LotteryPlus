a9b037bcb153a714af6b267cd18d7d3c1c6abe04
Chaka15205 <minecraftandmore86@gmail.com>
2015-06-24 12:44:55 -0400

    I tried

--- src/main/java/com/chaka15205/lotteryplus/lottery/LotteryManager.java
+++ src/main/java/com/chaka15205/lotteryplus/lottery/LotteryManager.java
@@ -1,8 +1,8 @@
 package com.chaka15205.lotteryplus.lottery;
 
+import com.chaka15205.lotteryplus.LotteryPlus;
 import com.chaka15205.lotteryplus.config.Config;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -10,8 +10,7 @@ import java.util.UUID;
 public class LotteryManager {
 
     private static LotteryManager lm;
-
-    private LotteryManager() {}
+    private static LotteryPlus plugin;
 
     public static LotteryManager getManager() {
         if (lm == null) {
@@ -23,13 +22,8 @@ public class LotteryManager {
     public void createLottery(UUID uuid, String name) {
         List<UUID> list = new ArrayList<UUID>();
         list.add(uuid);
-        Config.getLottery().addDefault(name, null);
-        Config.getLottery().addDefault(name + ".players", list);
+        Config.getLottery().set(name + ".players", list);
         Config.getLottery().options().copyDefaults(true);
-        try {
-            Config.getLottery().save(Config.getLotteryFile());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Config.saveLotteryFile();
     }
 }
